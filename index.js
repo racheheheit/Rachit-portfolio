81d5cd521257d2e29eb839eb7be9f38237bc7c7f
racheheheit <ranarachit06@gmail.com>
2025-07-19 22:49:52 +0530

    Add files via upload
    added files

---
+++ index.js
@@ -0,0 +1,23 @@
+
+//   function setMode(mode) {
+//     localStorage.setItem("userMode", mode);
+//   }
+
+
+//    window.onload = function () {
+//     const mode = localStorage.getItem("userMode");
+
+//     if (!mode) return;
+//  const mainSection = document.getElementById("main-content");
+//     const backgrounds = {
+//       stalker: "url('stalk.jpg')",
+//       developer: "url('develop.jpeg')",
+//       recruiter: "url('images/bg-recruiter.jpg')",
+//       designer: "url('images/bg-designer.jpg')"
+//     };
+
+
+//    if (mainSection && backgrounds[mode]) {
+//     mainSection.style.background = `url("${backgrounds[mode]}")`;
+//   }
+//    }
